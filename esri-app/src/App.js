d4f16f2062683faaefce10595921074fdac1827a
Rano <rmarufova2@washcoll.edu>
2024-05-21 12:07:59 -0400

    updates

--- esri-app/src/App.js
+++ esri-app/src/App.js
@@ -1,31 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import { MapView } from './components/MapView';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <MapView></MapView>
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import Map from './components/MapView';
 import './App.css'
@@ -33,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Esri Map App</h1>
+        <h1>DDEO School Locator</h1>
       </header>
-      <Map />
+      <Map/>
     </div>
   );
 }
--- esri-app/src/components/MapView.js
+++ esri-app/src/components/MapView.js
@@ -1,155 +1,3 @@
-// import React, { useRef, useEffect, useState } from 'react';
-// import { loadModules } from 'esri-loader';
-// import '../App.css'
-
-// const MapView = () => {
-//   const mapRef = useRef(null);
-//   const [intersectingFeatures, setIntersectingFeatures] = useState([]);
-//   const [selectedFeature, setSelectedFeature] = useState(null);
-
-//   useEffect(() => {
-//     loadModules([
-//       'esri/Map',
-//       'esri/views/MapView',
-//       'esri/widgets/Search',
-//       'esri/layers/FeatureLayer',
-//       'esri/Graphic',
-//       'esri/symbols/PictureMarkerSymbol'
-//     ], { css: true }).then(([Map, MapView, Search, FeatureLayer, Graphic, PictureMarkerSymbol]) => {
-//       const map = new Map({
-//         basemap: 'streets-navigation-vector',
-//       });
-
-//       const view = new MapView({
-//         container: mapRef.current,
-//         map: map,
-//         center: [-75.5644, 39.2096], // Longitude, latitude
-//         zoom: 7,
-//       });
-
-//       const featureLayers = [
-//         new FeatureLayer({
-//           url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternKindergarten_24_25_69ef0/FeatureServer',
-//           outFields: ['*'],
-//         }),
-//         new FeatureLayer({
-//           url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeEight_24_25/FeatureServer',
-//           outFields: ['*'],
-//         }),
-
-//         // Add more feature layers here as needed
-//       ];
-
-//       featureLayers.forEach(layer => map.add(layer));
-
-//       const searchWidget = new Search({
-//         view: view,
-//       });
-
-//       view.ui.add(searchWidget, {
-//         position: 'top-right',
-//       });
-
-//       searchWidget.on('search-complete', (event) => {
-//         const point = event.results[0].results[0].feature.geometry;
-//         view.goTo({
-//           center: point,
-//           zoom: 12,
-//         });
-//         addPinpoint(point);
-//         queryFeatures(point);
-//       });
-
-//       view.on('click', (event) => {
-//         view.goTo({
-//           center: event.mapPoint,
-//           zoom: 12,
-//         });
-//         addPinpoint(event.mapPoint);
-//         queryFeatures(event.mapPoint);
-//       });
-
-//       const queryFeatures = (point) => {
-//         let allIntersectingFeatures = [];
-//         let allQueries = featureLayers.map((layer) => {
-//           const query = layer.createQuery();
-//           query.geometry = point;
-//           query.spatialRelationship = 'intersects';
-//           query.returnGeometry = true;
-//           query.outFields = ['*'];
-//           return layer.queryFeatures(query).then((results) => {
-//             if (results.features.length > 0) {
-//               allIntersectingFeatures = [...allIntersectingFeatures, ...results.features];
-//             }
-//           });
-//         });
-
-//         Promise.all(allQueries).then(() => {
-//           setIntersectingFeatures(allIntersectingFeatures);
-//           if (allIntersectingFeatures.length > 0) {
-//             setSelectedFeature(allIntersectingFeatures[0]);
-//           } else {
-//             setSelectedFeature(null);
-//           }
-//         });
-//       };
-
-//       const addPinpoint = (point) => {
-//         const symbol = new PictureMarkerSymbol({
-//           url: 'https://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png',
-//           width: '24px',
-//           height: '24px'
-//         });
-
-//         const graphic = new Graphic({
-//           geometry: point,
-//           symbol: symbol
-//         });
-
-//         view.graphics.removeAll();
-//         view.graphics.add(graphic);
-//       };
-
-//     }).catch(err => {
-//       console.error(err);
-//     });
-//   }, []);
-
-//   const renderFeatureDetails = (feature) => {
-//     const fieldsToDisplay = ["SchoolName", "ReportCard", "Phone"];
-//     return (
-//       <div>
-//         <h4>Feature Details</h4>
-//         <ul>
-//           {fieldsToDisplay.map((key, index) => (
-//             <li key={index}>
-//               <strong>{key}:</strong> {feature.attributes[key]}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   };
-
-//   return (
-//     <div style={{ display: 'flex', height: '100vh', width: '100%' }}>
-//       <div ref={mapRef} style={{ flex: 1 }}></div>
-//       <div style={{ width: '300px', padding: '10px', overflowY: 'auto' }}>
-//         <h3>Intersecting Features</h3>
-//         <ul>
-//           {intersectingFeatures.map((feature, index) => (
-//             <li key={index} onClick={() => setSelectedFeature(feature)}>
-//               {feature.attributes.SchoolName || `Feature ${index + 1}`}
-//             </li>
-//           ))}
-//         </ul>
-//         {selectedFeature && renderFeatureDetails(selectedFeature)}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MapView;
 
 import React, { useRef, useEffect, useState } from 'react';
 import { loadModules } from 'esri-loader';
@@ -180,17 +28,23 @@ const MapView = () => {
         zoom: 7,
       });
 
-      const layerConfigs = [
-        {
-          url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternKindergarten_24_25_69ef0/FeatureServer',
-          name: 'Kindergarten'
-        },
-        {
-          url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeEight_24_25/FeatureServer',
-          name: '8th Grade'
-        },
+      //Disabling Pop-ups
+      view.popupEnabled = false;
 
-        // Add more layer configs here as needed
+      const layerConfigs = [
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternKindergarten_24_25_69ef0/FeatureServer', name: 'Kindergarten' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeOne_24_25/FeatureServer', name: '1st Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeTwo_24_25_10bfe/FeatureServer', name: '2nd Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeThree_24_25/FeatureServer', name: '3rd Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeFour_24_25/FeatureServer', name: '4th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeFive_24_25/FeatureServer', name: '5th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeSix_24_25/FeatureServer', name: '6th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeSeven_24_25/FeatureServer', name: '7th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeEight_24_25/FeatureServer', name: '8th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeNine_24_25/FeatureServer', name: '9th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/DE_SchoolsFeederPatternGradeTen2425_ExportFeatures/FeatureServer', name: '10th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/FeederPatternGradeEleven_24_25/FeatureServer', name: '11th Grade' },
+        { url: 'https://services2.arcgis.com/M7TEANoOZzgrO5AX/arcgis/rest/services/Feeder_Pattern_Grade_Twelve_24_25_2209f/FeatureServer', name: '12th Grade' },
       ];
 
       const featureLayers = layerConfigs.map(config => {
@@ -235,7 +89,11 @@ const MapView = () => {
         });
         addPinpoint(point);
         queryFeatures(point);
+
+        // Disable pop-ups on search
+        view.popup.clear();
       });
+
 
       view.on('click', (event) => {
         view.goTo({
@@ -276,15 +134,25 @@ const MapView = () => {
     });
   }, []);
 
- 
+
+  
+  const sortedIntersectingFeatures = Object.keys(intersectingFeatures)
+    .sort((a, b) => {
+      const layerOrder = [
+        'Kindergarten', '1st Grade', '2nd Grade', '3rd Grade', '4th Grade',
+        '5th Grade', '6th Grade', '7th Grade', '8th Grade', '9th Grade',
+        '10th Grade', '11th Grade', '12th Grade'
+      ];
+      return layerOrder.indexOf(a) - layerOrder.indexOf(b);
+    });
 
   return (
     <div style={{ display: 'flex', height: '100vh', width: '100%' }}>
       <div ref={mapRef} style={{ flex: 1 }}></div>
-      <div style={{ width: '300px', padding: '10px', overflowY: 'auto' }}>
-        <h3>Map Application</h3>
-        <h3>Intersecting Features</h3>
-        {Object.keys(intersectingFeatures).map((layerName, index) => (
+      <div className='panel'>
+        <h3>Locate your School</h3>
+        <p>Click your adress on the map or use the search bar</p>
+        {sortedIntersectingFeatures.map((layerName, index) => (
           <div key={index}>
             <table className="feature-list">
               <thead>
@@ -300,13 +168,13 @@ const MapView = () => {
                         <strong>School Name:</strong> {feature.attributes.SchoolName || `Feature ${featureIndex + 1}`}
                       </div>
                       <div>
-                        <strong>Report Card:</strong> <a href={feature.attributes.Report_Card}>{'Click to view'}</a>
+                        <strong>Report Card:</strong> <a href={feature.attributes.Report_Card} target="_blank" rel="noopener noreferrer" title="View Report Card">Click to view</a>
                       </div>
                       <div>
                         <strong>Phone:</strong> {feature.attributes.Phone}
                       </div>
                       <div>
-                        <strong>Website:</strong> <a href={feature.attributes.WebAddress}>{'Click to view'}</a>
+                        <strong>Website:</strong> <a href={feature.attributes.WebAddress} target="_blank" rel="noopener noreferrer" title="View Report Card">Click to view</a>
                       </div>
                     </td>
                   </tr>
@@ -315,6 +183,7 @@ const MapView = () => {
             </table>
           </div>
         ))}
+        
       </div>
     </div>
   );
